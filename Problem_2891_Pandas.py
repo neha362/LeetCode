7335534d94b63e3a99d06e7655eac77738013f0d
neha362 <80128621+neha362@users.noreply.github.com>
2025-02-16 15:36:49 -0500

    Create Problem_2891_Pandas.py

---
+++ Problem_2891_Pandas.py
@@ -0,0 +1,5 @@
+#Method Chaining, Feb 13 2025
+import pandas as pd
+
+def findHeavyAnimals(animals: pd.DataFrame) -> pd.DataFrame:
+    return animals[animals["weight"] > 100].sort_values(by = ["weight"], ascending=False)[["name"]]
