321ed0236a96fe21a8035ab12897bb86d8ac0f17
neha362 <80128621+neha362@users.noreply.github.com>
2025-02-16 12:39:07 -0500

    Create Problem_2342_Python.py

---
+++ Problem_2342_Python.py
@@ -0,0 +1,15 @@
+#Max Sum of a Pair With Equal Sum of Digits, solved Jan 13 2025
+class Solution:
+    def maximumSum(self, nums: List[int]) -> int:
+        digitMap = [0] * 82
+        res = -1
+        for i in nums:
+            count = 0
+            j = i
+            while j > 0:
+                j, k = divmod(j, 10)
+                count += k
+            if digitMap[count] > 0:
+                res = max(res, digitMap[count] + i)
+            digitMap[count] = max(digitMap[count], i)
+        return res
