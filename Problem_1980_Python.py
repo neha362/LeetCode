9e2ea3afe6463cf294b148fd4d083038a68e813e
neha362 <80128621+neha362@users.noreply.github.com>
2025-02-20 16:05:10 -0500

    Create Problem_1980_Python.py

---
+++ Problem_1980_Python.py
@@ -0,0 +1,4 @@
+#Find Unique Binary String, solved Feb 19 2025
+class Solution:
+    def findDifferentBinaryString(self, nums: List[str]) -> str:
+        return "".join(["0" if nums[i][i] == "1" else "1" for i in range(len(nums))])
